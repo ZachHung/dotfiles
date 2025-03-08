e16d0ea7199da8a31143df86f584b936278eb642
ZachHung <zachhung911@gmail.com>
2025-03-08 00:28:42 +0000

    chore: update fisher packages

--- .config/fish/functions/pnpm.fish
+++ .config/fish/functions/pnpm.fish
@@ -1,6 +1,5 @@
 set -gx PNPM_HOME ~/.pnpm
-set -g PNPM_DEFAULT_GLOBAL_DIR ~/.local/share/pnpm-global
-set -g PNPM_DEFAULT_STORE_DIR ~/.local/share/pnpm-store
+set -g PNPM_GLOBAL_DIR ~/.local/share/pnpm-global
 
 function pnpm
     set -l non_opts
@@ -12,12 +11,10 @@ function pnpm
     if test "$non_opts[2]" = pnpm
         switch "$non_opts[1]"
             case i install
-                _pnpm_ensure_deps install
                 _pnpm_install_latest
-                set PATH $PNPM_HOME:$PATH
+                set PATH $PNPM_HOME:$PATH # source pnpm after install
                 return
             case up update upgrade
-                _pnpm_ensure_deps update
                 _pnpm_install_latest
                 return
             case rm uninstall un
@@ -34,52 +31,21 @@ function pnpm
         end
     end
 
-    set -l args $argv
-    argparse -i g/global -- $argv
-
-    if set -q _flag_global
-        PATH=(nvm-which 20)/bin:$PATH $PNPM_HOME/pnpm $args
-    else if type -q node && test (node -v | string match -r '\d+') -lt 16
-        PATH=(nvm-which 20)/bin:$PATH $PNPM_HOME/pnpm -s dlx pnpm@7 $args
-    else
-        PATH=(nvm-which 20)/bin:$PATH $PNPM_HOME/pnpm $args
-    end
-end
-
-function _pnpm_ensure_deps -a action
-    set -l deps jorgebucaran/nvm.fish dangh/nvm-which.fish
-    switch "$action"
-        case install
-            set -l missing_deps
-            for dep in $deps
-                fisher ls | grep -q $dep || set -a missing_deps $dep
-            end
-            test -n "$missing_deps" && fisher install $missing_deps
-        case update
-            fisher update $deps
-    end
+    $PNPM_HOME/pnpm $argv
 end
 
 function _pnpm_install_latest
     echo (set_color magenta)Installing pnpm@latest(set_color normal)
 
-    set -l global_dir (command npm config get global-dir)
-    set -l store_dir (command npm config get store-dir)
-
-    contains "$global_dir" '' undefined && begin
-        set global_dir $PNPM_DEFAULT_GLOBAL_DIR
-        command npm config set global-dir $global_dir
-    end
-    contains "$store_dir" '' undefined && begin
-        set store_dir $PNPM_DEFAULT_STORE_DIR
-        command npm config set store-dir $store_dir
-    end
-
     # prepend global-dir to PATH to expose global binaries
-    fish_add_path $global_dir/bin
+    mkdir -p $PNPM_GLOBAL_DIR/bin
+    fish_add_path $PNPM_GLOBAL_DIR/bin
 
     # install pnpm
     curl --location --progress-bar https://get.pnpm.io/install.sh | sh -
+
+    # install nodejs
+    $PNPM_HOME/pnpm env use -g latest
 end
 
 function _pnpm_uninstall
@@ -91,32 +57,10 @@ function _pnpm_uninstall
         _pnpm_remove_path $PNPM_HOME
     end 2>/dev/null
 
-    set -l global_dir (command npm config get global-dir)
-    if not contains "$global_dir" '' undefined
-        command npm config delete global-dir
-    else
-        set global_dir $PNPM_DEFAULT_GLOBAL_DIR
-    end
-    test -d "$global_dir" && _pnpm_confirm -n (set_color red)"Delete "(set_color -u)"$global_dir"(set_color normal)(set_color red)"?"(set_color normal) && rm -rf "$global_dir"
-    switch $global_dir
-        case '' undefined
-        case \*
-            _pnpm_remove_path $global_dir/bin
-    end
+    _pnpm_remove_path $PNPM_GLOBAL_DIR/bin
 
-    set -l global_bin_dir (command npm config get global-bin-dir)
-    if not contains "$global_bin_dir" '' undefined
-        command npm config delete global-bin-dir
-        test -d "$global_bin_dir" && rm -rf "$global_bin_dir"
-    end
-
-    set -l store_dir (command npm config get store-dir)
-    if not contains "$store_dir" '' undefined
-        command npm config delete store-dir
-    else
-        set store_dir $PNPM_DEFAULT_STORE_DIR
-    end
-    test -d "$store_dir" && _pnpm_confirm -n (set_color red)'Delete '(set_color -u)"$store_dir"(set_color normal)(set_color red)'?'(set_color normal) && rm -rf "$store_dir"
+    test -d "$PNPM_GLOBAL_DIR" && _pnpm_confirm -n (set_color red)"Delete "(set_color -u)"$PNPM_GLOBAL_DIR"(set_color normal)(set_color red)"?"(set_color normal) && rm -rf "$PNPM_GLOBAL_DIR"
+    test -d "$PNPM_HOME" && _pnpm_confirm -n (set_color red)'Delete '(set_color -u)"$PNPM_HOME"(set_color normal)(set_color red)'?'(set_color normal) && rm -rf "$PNPM_HOME"
 end
 
 function _pnpm_confirm
